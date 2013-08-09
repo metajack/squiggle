4e66d27cb4fe67e99b9675c0d7aa6cc3b62e45fd
Jack Moffitt <jack@metajack.im>
2013-08-08 21:42:41 -0600

    sketch some traits

--- src/program.rs
+++ src/program.rs
@@ -1,4 +1,11 @@
-enum UnaOp {
+use std::rand::{Rand, Rng};
+
+pub struct Program {
+    id: ~str,
+    expr: ~Expr,
+}
+
+pub enum UnaOp {
     Not,
     Shl1,
     Shr1,
@@ -6,14 +13,14 @@ enum UnaOp {
     Shr16,
 }
 
-enum BinOp {
+pub enum BinOp {
     And,
     Or,
     Xor,
     Plus,
 }
 
-enum Expr {
+pub enum Expr {
     Zero,
     One,
     Ident(~str),
@@ -22,8 +29,31 @@ enum Expr {
     Op2(BinOp, ~Expr, ~Expr),
 }
 
-impl FromStr for ~Expr {
-    pub fn from_str(s: &str) -> Option<~Expr> {
+impl FromStr for Program {
+    pub fn from_str(s: &str) -> Option<Program> {
         None
+    }
+}
+
+impl ToStr for Program {
+    pub fn to_str(&self) -> ~str {
+        let mut program = ~"(lambda (";
+        program.push_str(self.id);
+        program.push_str(") ");
+        program.push_str(self.expr.to_str());
+        program
+    }
+}
+
+impl ToStr for Expr {
+    pub fn to_str(&self) -> ~str {
+        // serializer
+        ~""
+    }
+}
+
+impl Rand for Expr {
+    pub fn rand<R: Rng>(rng: &mut R) -> Expr {
+        Zero
     }
 }
