38f9ddaf85430c695813f6f899b7206810c7f388
Huon Wilson <dbau.pp+github@gmail.com>
2013-08-09 16:54:47 +1000

    eval fold bench, eval test.

--- src/eval.rs
+++ src/eval.rs
@@ -125,4 +125,73 @@ mod test {
             }
         }
     }
+    #[bench]
+    fn bench_eval_fold(bh: &mut BenchHarness) {
+        let fold_expr = ~Fold {
+            foldee: ~Ident(~"x"),
+            init: ~Zero,
+            next_id: ~"n",
+            accum_id: ~"a",
+            body: ~Op2(Plus, ~Ident(~"n"), ~Ident(~"a"))
+        };
+        let prog = Program::new(~"x", fold_expr);
+        let mut rng = rand::rng();
+
+        do bh.iter {
+            prog.eval(rng.gen());
+        }
+    }
+
+    #[test]
+    fn test_eval() {
+        let fold_expr = ~Fold {
+            foldee: ~Ident(~"x"),
+            init: ~Zero,
+            next_id: ~"n",
+            accum_id: ~"a",
+            body: ~Op2(Plus, ~Ident(~"n"), ~Ident(~"a"))
+        };
+        let fold_fn = |mut x: u64| {
+            let mut accum = 0;
+            for _ in range(0, 8) {
+                accum += x & 0xff;
+                x >>= 8;
+            }
+            accum
+        };
+
+        let progs_fn: ~[(Program, &fn(u64) -> u64)] = ~[
+                        (Program::new(~"x", ~Zero), |_| 0),
+                        (Program::new(~"x", ~One), |_| 1),
+                        (Program::new(~"x", ~Ident(~"x")), |x| x),
+                        (Program::new(~"x", ~Op1(Not, ~Ident(~"x"))), |x| !x),
+                        (Program::new(~"x", ~Op1(Shl1, ~Ident(~"x"))), |x| x << 1),
+                        (Program::new(~"x", ~Op1(Shr1, ~Ident(~"x"))), |x| x >> 1),
+                        (Program::new(~"x", ~Op1(Shr4, ~Ident(~"x"))), |x| x >> 4),
+                        (Program::new(~"x", ~Op1(Shr16, ~Ident(~"x"))), |x| x >> 16),
+                        (Program::new(~"x", ~Op2(And, ~Ident(~"x"), ~One)), |x| x & 1),
+                        (Program::new(~"x", ~Op2(Or, ~Ident(~"x"), ~One)), |x| x | 1),
+                        (Program::new(~"x", ~Op2(Xor, ~Ident(~"x"), ~One)), |x| x ^ 1),
+                        (Program::new(~"x", ~Op2(Plus, ~Ident(~"x"), ~One)), |x| x + 1),
+                        (Program::new(~"x", ~If0(~Ident(~"x"), ~One, ~Zero)),
+                         |x| if x == 0 {1} else {0}),
+                        (Program::new(~"x", ~If0(~Zero, ~One, ~Zero)), |_| 1),
+                        (Program::new(~"x", ~If0(~One, ~One, ~Zero)), |_| 0),
+                        (Program::new(~"x", fold_expr), fold_fn),
+                        ];
+
+        for (p, f) in progs_fn.consume_iter() {
+            info!(p.to_str());
+
+            // some "special cases", maybe.
+            assert_eq!(p.eval(0), f(0));
+            assert_eq!(p.eval(1), f(1));
+            assert_eq!(p.eval(-1), f(-1));
+
+            for _ in range(0, 100) {
+                let i: u64 = rand::random();
+                assert_eq!(p.eval(i), f(i));
+            }
+        }
+    }
 }
