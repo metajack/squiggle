51507a82e7af51e37758cc5de3ac2fc293f789bd
Brendan Zabarauskas <bjzaba@yahoo.com.au>
2013-08-09 16:56:47 +1000

    Add Parser::skip_ws method

--- src/parse.rs
+++ src/parse.rs
@@ -8,49 +8,70 @@ pub trait Parse {
 
 impl<'self> Parse for &'self str {
     pub fn parse(&self) -> ParseResult {
-        Parser {
-            src_pos: 0,
-            src: *self,
-        }.parse()
+        Parser::new(*self).parse()
     }
 }
 
 pub struct Parser<'self> {
-    priv src_pos: uint,
-    priv src: &'self str,
-}
-
-impl<'self> Parse for Parser<'self> {
-    pub fn parse(&self) -> ParseResult {
-        self.consume_lambda()
-    }
+    pos: uint,
+    src: &'self str,
 }
 
 impl<'self> Parser<'self> {
-    pub fn consume_ws(&self) -> ParseResult {
-        Err(~"Not yet implemented.")
+    pub fn new<'r>(src: &'r str) -> Parser<'r> {
+        Parser {
+            pos: 0,
+            src: src,
+        }
     }
-    pub fn consume_str(&self, _expected: &str) -> ParseResult {
+
+    pub fn parse(&mut self) -> ParseResult {
+        self.consume_lambda()
+    }
+
+    pub fn skip_ws(&mut self) {
+        for c in self.src.iter() {
+            if c.is_whitespace() { self.pos += 1 } else { break };
+        }
+    }
+
+    pub fn consume_ws(&mut self) -> ParseResult {
         Err(~"Not yet implemented.")
     }
 
-    pub fn consume_lambda(&self) -> ParseResult {
+    pub fn consume_str(&mut self, _expected: &str) -> ParseResult {
         Err(~"Not yet implemented.")
     }
 
-    pub fn comsume_expression(&self) -> ParseResult {
+    pub fn consume_lambda(&mut self) -> ParseResult {
         Err(~"Not yet implemented.")
     }
 
-    pub fn consume_op1(&self) -> ParseResult {
+    pub fn comsume_expression(&mut self) -> ParseResult {
         Err(~"Not yet implemented.")
     }
 
-    pub fn consume_op2(&self) -> ParseResult {
+    pub fn consume_op1(&mut self) -> ParseResult {
         Err(~"Not yet implemented.")
     }
 
-    pub fn consume_id(&self) -> ParseResult {
+    pub fn consume_op2(&mut self) -> ParseResult {
         Err(~"Not yet implemented.")
+    }
+
+    pub fn consume_id(&mut self) -> ParseResult {
+        Err(~"Not yet implemented.")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_skip_ws() {
+        let mut p = Parser::new(" \t\n");
+        p.skip_ws();
+        assert_eq!(p.pos, 3);
     }
 }
